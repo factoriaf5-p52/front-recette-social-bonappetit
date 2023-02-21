86b089e49a97ff28171edd00e48c36d325471fa1
Rocio <rgermillaclopez@gmail.com>
2023-02-21 11:22:34 +0100

    statistic and recipes list pages

--- src/pages/RecipeListPage.tsx
+++ src/pages/RecipeListPage.tsx
@@ -1,9 +1,94 @@
 import React from "react";
+import pasta from "../assets/pasta.svg";
+import dessert from "../assets/dessert.svg";
+import meatballs from "../assets/meatballs.svg";
+import burguer from "../assets/burguer.svg";
+import salad from "../assets/salad.svg"; 
+import salmon from "../assets/salmon.svg";
 
 type Props = {};
 
 const RecipeListPage = (props: Props) => {
-  return <div>RecipeListPage</div>;
+  return (
+  <>
+    
+        <h1 className="font-bold text-center p-4">Recipes List</h1>
+        <form className="max-w-7xl flex flex-col justify-center items-center px-20 py-5">
+          <div className="flex flex-wrap gap-3">
+              <input className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Recipe name..." type="text" name="search"/>
+              <input className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Meal type..." type="text" name="search"/>
+              <input className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Rating..." type="text" name="search"/>
+              <button className="bg-red w-fit rounded-md py-2 pl-3 pr-3 focus:outline-none text-white sm:text-sm">Search</button>
+          </div>
+        </form>
+
+        <h2 className="font-medium text-center">Results</h2>
+
+        <div className="flex flex-wrap justify-center items-center px-20 py-5">
+          
+          <div className="flex flex-col text-center mt-4">
+            <img src={pasta} alt="pasta" className="scale-150 p-6"/>
+            <h3 className="font-medium">Pasta</h3>
+          </div>
+        
+          <div className="flex flex-col text-center mt-4">
+            <img src={dessert} alt="dessert" className="scale-150 p-6"/>
+            <h3 className="font-medium">Dessert</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={meatballs} alt="meatballs" className="scale-150 p-6"/>
+            <h3 className="font-medium">Meatballs</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={pasta} alt="pasta" className="scale-150 p-6" />
+            <h3 className="font-medium">Pasta</h3>
+          </div>
+          
+          <div className="flex flex-col text-center mt-4">
+            <img src={dessert} alt="dessert" className="scale-150 p-6"/>
+            <h3 className="font-medium">Dessert</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={meatballs} alt="meatballs" className="scale-150 p-6"/>
+            <h3 className="font-medium">Meatballs</h3>
+          </div> 
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={burguer} alt="burguer" className="scale-150 p-6"/>
+            <h3 className="font-medium">Burguer</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={salad} alt="salad" className="scale-150 p-6"/>
+            <h3 className="font-medium">Salad</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={salmon} alt="salmon" className="scale-150 p-6"/>
+            <h3 className="font-medium">Salmon</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={burguer} alt="burguer" className="scale-150 p-6"/>
+            <h3 className="font-medium">Burguer</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={salad} alt="salad" className="scale-150 p-6"/>
+            <h3 className="font-medium">Salad</h3>
+          </div>
+
+          <div className="flex flex-col text-center mt-4">
+            <img src={salmon} alt="salmon" className="scale-150 p-6"/>
+            <h3 className="font-medium">Salmon</h3>
+          </div>
+        </div>
+      
+  </>
+);
 };
 
 export default RecipeListPage;
--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -1,7 +1,51 @@
+import pasta from "../assets/pasta.svg"
+import dessert from "../assets/dessert.svg"
+import meatballs from "../assets/meatballs.svg"
+import meat from "../assets/meat.svg"
+import tarta from "../assets/tarta.svg"
+import donnuts from "../assets/donnuts.svg"
+import sushi from "../assets/sushi.svg" 
+import cake from "../assets/cake.svg" 
+import dip from "../assets/dip.svg" 
+import burguer from "../assets/burguer.svg"
+import salad from "../assets/salad.svg" 
+import salmon from "../assets/salmon.svg"
+
 type Props = {};
 
 const StatisticsPage = (props: Props) => {
-  return <div>StatisticsPage</div>;
+  return (
+  <>
+    <h1 className="font-bold text-center p-4">Statistics</h1>
+      <div className="max-w-7xl flex flex-col justify-center items-center">
+        <p className="font-bold text-left">Best rated recipes</p>
+          <div className="grid grid-cols-3 gap-4 p-3">
+            <img src={pasta} alt="pasta"/>
+            <img src={dessert} alt="dessert"/>
+            <img src={meatballs} alt="meatballs"/>
+          </div>
+        <p className="font-bold">The most viewed</p>
+          <div className="grid grid-cols-3 gap-4 p-3">
+            <img src={meat} alt="meat"/>
+            <img src={tarta} alt="tarta"/>
+            <img src={donnuts} alt="donnuts"/>
+          </div>
+        <p className="font-bold">Trending recipes</p>
+          <div className="grid grid-cols-3 gap-4 p-3">
+            <img src={sushi} alt="sushi"/>
+            <img src={cake} alt="cake"/>
+            <img src={dip} alt="dips"/>
+          </div>
+        <p className="font-bold">Chef's recommendations</p>
+          <div className="grid grid-cols-3 gap-4 p-3">
+            <img src={burguer} alt="burguer"/>
+            <img src={salad} alt="salad"/>
+            <img src={salmon} alt="salmon"/>
+          </div>
+      </div>
+  </>
+  );
+
 };
 
 export default StatisticsPage;
