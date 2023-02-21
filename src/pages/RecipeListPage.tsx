72b915a886a95530e690934d3ada72a894ba73db
Rocio <rgermillaclopez@gmail.com>
2023-02-21 15:08:03 +0100

    connection db

--- src/pages/RecipeListPage.tsx
+++ src/pages/RecipeListPage.tsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import pasta from "../assets/pasta.svg";
 import dessert from "../assets/dessert.svg";
 import meatballs from "../assets/meatballs.svg";
 import burguer from "../assets/burguer.svg";
 import salad from "../assets/salad.svg"; 
 import salmon from "../assets/salmon.svg";
+import axios from "axios";
 
 type Props = {};
 
 const RecipeListPage = (props: Props) => {
+  const [title, setTitle]= useState("");
+  const [type, setType]= useState("");
+  const [list, setList]= useState([]);
+  async function handleSubmit(e:any){ 
+    try {
+      e.preventDefault();
+      const result = await axios.get(`https://backend-bonappetit.up.railway.app//api/v1/recipes/title/${value}`)
+      setList(()=> result.data.length > 0?
+      result.data:[{name:'Recipes not found'}]);
+      console.log(list);
+      } 
+      catch (error) {
+      console.log(error);
+
+    }
+
+  }
   return (
   <>
     
         <h1 className="font-bold text-center p-4">Recipes List</h1>
         <form className="max-w-7xl flex flex-col justify-center items-center px-20 py-5">
           <div className="flex flex-wrap gap-3">
-              <input className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Recipe name..." type="text" name="search"/>
-              <input className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Meal type..." type="text" name="search"/>
-              <input className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Rating..." type="text" name="search"/>
-              <button className="bg-red w-fit rounded-md py-2 pl-3 pr-3 focus:outline-none text-white sm:text-sm">Search</button>
+              <input onChange={(e)=>{setTitle(e.target.value)}} className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Recipe name..." type="text" name="search"/>
+              <input onChange={(e)=>{setType(e.target.value)}} className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Meal type..." type="text" name="search"/>
+              <button onClick={handleSubmit} className="bg-red w-fit rounded-md py-2 pl-3 pr-3 focus:outline-none text-white sm:text-sm">Search</button>
           </div>
         </form>
 
