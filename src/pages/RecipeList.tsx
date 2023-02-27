98b16307b9f296d0ae6258ff3a72c73f04cfd075
Sablinita <sablinahare@gmail.com>
2023-02-28 00:03:34 +0100

    changed button and placeholders size

--- src/pages/RecipeList.tsx
+++ src/pages/RecipeList.tsx
@@ -23,14 +23,14 @@ const RecipeListPage = (props: Props) => {
 
   return (
     <>
-      <h1 className="font-bold text-center p-4">Recipes List</h1>
+      <h1 className="font-bold text-center p-4 text-2xl">Recipes List</h1>
       <form className="max-w-7xl flex flex-col justify-center items-center px-20 py-5">
         <div className="flex flex-wrap gap-3">
           <input
             onChange={(e) => {
               setTitle(e.target.value);
             }}
-            className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            className="placeholder:italic placeholder:text-slate-400 block w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             placeholder="Recipe name..."
             type="text"
             name="search"
@@ -39,21 +39,21 @@ const RecipeListPage = (props: Props) => {
             onChange={(e) => {
               setType(e.target.value);
             }}
-            className="placeholder:italic placeholder:text-slate-400 block w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            className="placeholder:italic placeholder:text-slate-400 block w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             placeholder="Meal type..."
             type="text"
             name="search"
           />
           <button
             onClick={handleSubmit}
-            className="bg-red w-fit rounded-md py-2 pl-3 pr-3 focus:outline-none text-white sm:text-sm"
+            className="bg-red w-64 rounded-md py-2 pl-3 pr-3 focus:outline-none text-white sm:text-sm"
           >
             Search
           </button>
         </div>
       </form>
 
-      <h2 className="font-medium text-center mt-6">Results</h2>
+      <h2 className="font-medium text-center mt-6 font-bold text-xl">Results</h2>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-6 mt-8 center-center gap-20">
         <div className="flex flex-col items-center">
