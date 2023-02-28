5a4024b3601e54c7fdf1dfe0fdda2de9153e5129
Joel Herrero <joelv2@gmail.com>
2023-02-28 14:20:56 +0100

    recipe detail first push

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { useParams, Params } from "react-router-dom";
 
 type Props = {};
 
-const RecipeDetailPage = (props: Props) => {
-  return <div>RecipeDetailPage</div>;
+const RecipeDetailPage = () => {
+  const { id } = useParams<Params>();
+
+  return (
+    <section className="w-full flex justify-center items-center">
+      <div className="max-w-screen-lg bg-green">
+        <h1>title</h1>
+        <div>
+          <img src="" alt="" />
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <div>Views: </div>
+          <div>Type: </div>
+          <div>Difficulty: </div>
+          <div>Time: </div>
+        </div>
+        <p>Description</p>
+        <p>Lorem Description</p>
+        <div>
+          <h1>Comments</h1>
+          <div>
+            <img src="" alt="comment-guy" />
+            <p>commentario</p>
+          </div>
+          <input type="text" placeholder="write here..." />
+        </div>
+        Recipe Detail Page for recipe {id}
+      </div>
+    </section>
+  );
 };
 
 export default RecipeDetailPage;
--- src/services/recipeServices.tsx
+++ src/services/recipeServices.tsx
@@ -1,27 +1,15 @@
 import axios from "axios";
 
+const URL_API = "https://backend-bonappetit.up.railway.app/api/v1/recipes/";
 
-const URL_API =
-  "https://backend-bonappetit.up.railway.app/api/v1/recipes/title/";
-
-export const findAllRecipes = async (value: any) => {
+export const findAllRecipes = async () => {
   try {
-    const result = await axios.get(URL_API + value);
-    return result;
+    const result = await axios.get(URL_API);
+    console.log(result.data);
+    return result.data;
   } catch (error) {
     return error;
   }
 };
 
-// const Recettes = () => {
-
-//   const recipes = [
-//     {title: "Lemon Pie"},
-//     {title: "Pizza Margarita"}
-//   ]
-
-//   const [recette, setRecette] = React.useState
-// }
-
-
-
+export default { findAllRecipes };
