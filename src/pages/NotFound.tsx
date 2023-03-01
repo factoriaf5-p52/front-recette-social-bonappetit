011fea3cfdc86be859eda2df0f76d153db629933
Sablinita <sablinahare@gmail.com>
2023-03-01 09:00:49 +0100

    fixed the text in the center of not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 const NotFoundPage = (props: Props) => {
   return <div>
-    <h1 className="flex text-center font-bold text-2xl mt-10">Not Found</h1>
-    <h2 className="flex text-center font-bold text-xl mt-10">Page not found.</h2>
+    <h1 className="flex justify-center font-bold text-2xl mt-32">Not Found</h1>
+    <h2 className="flex justify-center font-bold text-xl mt-12">Page not found.</h2>
     </div>;
 };
 
