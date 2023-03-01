5057bcaec2cb43d12aa102c19e00b70d24bcc6c8
Joel Herrero <joelv2@gmail.com>
2023-03-01 12:05:35 +0100

    filters and images size fixed

--- src/pages/RecipeList.tsx
+++ src/pages/RecipeList.tsx
@@ -23,21 +23,45 @@ type Recipe = {
 const RecipeListPage = (props: Props) => {
   const [title, setTitle] = useState<string>("");
   const [type, setType] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<string>("");
   const [list, setList] = useState<Recipe[]>([]);
+  const [filteredList, setFilteredList] = useState<Recipe[]>([]);
 
-  async function handleSubmit(e: any) {
-    e.preventDefault();
+  function filterData(
+    searchTitleValue: any,
+    searchTypeValue: any,
+    searchDifficultyValue: any
+  ) {
+    const filteredData = list
+      .filter((item) =>
+        item.title.toLowerCase().includes(searchTitleValue.toLowerCase())
+      )
+      .filter((item) =>
+        item.mealType.toLowerCase().includes(searchTypeValue.toLowerCase())
+      )
+      .filter((item) =>
+        item.difficulty
+          .toLowerCase()
+          .includes(searchDifficultyValue.toLowerCase())
+      );
+    setFilteredList(filteredData);
   }
+
   useEffect(() => {
     recipeService
       .findAllRecipes()
       .then((data) => {
         setList(data);
+        setFilteredList(data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+  useEffect(() => {
+    filterData(title, type, difficulty);
+  }, [type, title, difficulty]);
 
   return (
     <>
@@ -49,26 +73,39 @@ const RecipeListPage = (props: Props) => {
               onChange={(e) => {
                 setTitle(e.target.value);
               }}
-              className="placeholder:italic placeholder:text-slate-400 block w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              className="bg-purewhite placeholder:italic placeholder:text-slate-400 block w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               placeholder="Recipe name..."
               type="text"
               name="search"
             />
-            <input
+            <select
+              defaultValue=""
+              className="w-64 border bg-purewhite rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               onChange={(e) => {
                 setType(e.target.value);
               }}
-              className="placeholder:italic placeholder:text-slate-400 block w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Meal type..."
-              type="text"
-              name="search"
-            />
-            <button
-              onClick={handleSubmit}
-              className="bg-red w-64 rounded-md py-2 pl-3 pr-3 focus:outline-none text-white sm:text-sm"
+              name="mealtype"
+              id="mealtype"
             >
-              Search
-            </button>
+              <option value=""> -- select meal type -- </option>
+              <option value="Breakfast">Breakfast</option>
+              <option value="Lunch">Lunch</option>
+              <option value="Dinner">Dinner</option>
+            </select>
+            <select
+              defaultValue=""
+              className="w-64 border bg-purewhite rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              onChange={(e) => {
+                setDifficulty(e.target.value);
+              }}
+              name="difficulty"
+              id="difficulty"
+            >
+              <option value=""> -- select difficulty -- </option>
+              <option value="Easy">Easy</option>
+              <option value="Average">Average</option>
+              <option value="Difficult">Difficult</option>
+            </select>
           </form>
 
           <h2 className="font-medium text-center mt-6 font-bold text-xl">
@@ -76,15 +113,15 @@ const RecipeListPage = (props: Props) => {
           </h2>
 
           <div className="flex items-center justify-center flex-wrap gap-10 mx-6 my-12">
-            {list.map((recipe, i) => (
+            {filteredList.map((recipe, i) => (
               <div key={i} className="flex flex-col w-32 items-center">
                 <img src={recipe.image} alt="pasta" className="w-{10}" />
-                <h3 className="font-medium mt-2 text-center">
+                <h3 className="font-bold mt-2 text-center">
                   <Link to={`/recipes/recipe/${recipe._id}`}>
                     {recipe.title}
                   </Link>
                 </h3>
-                <p className="font-medium">{recipe.mealType}</p>
+                <p className="font-medium italic">{recipe.mealType}</p>
               </div>
             ))}
           </div>
