e92914f3a82e798b0edecea37729cf676ee0a43d
Joel Herrero <joelv2@gmail.com>
2023-03-01 17:48:08 +0100

    first test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -5,10 +5,9 @@ import App from "../App";
 describe("Testing de Home page", () => {
   test("renders learn react link", () => {
     render(<App />);
-    expect(1 + 1).toBe(2);
-    // const element = screen.getByText(
-    //   /Discover the best chefs & recipes from around the world/i
-    // );
-    // expect(element).toBeInTheDocument();
+    const element = screen.getByText(
+      /Discover the best chefs & recipes from around the world/i
+    );
+    expect(element).toBeInTheDocument();
   });
 });
