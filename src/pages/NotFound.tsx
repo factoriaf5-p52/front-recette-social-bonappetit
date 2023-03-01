e9065235b6feb12ab30d8054c47195ae5f9bca18
Sablinita <sablinahare@gmail.com>
2023-03-01 12:18:28 +0100

    finished recipe-detail-page responsive

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Footer from "../components/Footer/Footer"
+
 
 type Props = {};
 
 const NotFoundPage = (props: Props) => {
-  return <div>
+  return (
+    <>
+    <div>
     <h1 className="flex justify-center font-bold text-2xl mt-32">Not Found</h1>
     <h2 className="flex justify-center font-bold text-xl mt-12">Page not found.</h2>
-    </div>;
+    </div>
+
+    <Footer/>
+    </>
+  )
 };
 
 export default NotFoundPage;
--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -40,12 +40,12 @@ const RecipeDetailPage = () => {
   }, []);
 
   return (
-    <section className="max-width:1280px  flex justify-center items-center">
-      <div className="flex flex-col justify-center bg-green">
-        <h1 className="flex font-bold text-2xl justify-center mt-2">Detail Recette</h1>
-        <h2 className="flex mt-2 mb-2 justify-center font-bold text-xl">{recipeDetail.title}</h2>
-        <div className="flex justify-center flex-row gap-8 ">
-          <img className="md-{10}"src={recipeDetail.image} alt="specialty img" />
+    <section className="max-width:1280px flex justify-center items-center">
+      <div className="flex flex-col justify-center h-full">
+        <h1 className="flex font-bold text-2xl justify-center mt-4">Detail Recette</h1>
+        <h2 className="flex mt-3 mb-3 justify-center font-bold text-xl">{recipeDetail.title}</h2>
+        <div className="flex justify-center flex-row gap-5 ">
+          <img className="h-60"src={recipeDetail.image} alt="specialty img" />
           <ul>
           <h3 className="mb-2 font-medium mt-8">Ingredients</h3>
             {recipeDetail.ingredients &&
@@ -61,33 +61,39 @@ const RecipeDetailPage = () => {
           </ul>
         </div>
         
-        <div className="flex justify-row justify-center gap-4 mt-4">
+        <div className="flex justify-row justify-center gap-3 mt-6 mr-3 ml-3">
+          <div className="flex flex-col">
           <img src={iconViews} alt="views" />
           <p>123</p>
+          </div>
+
+          <div className="flex flex-col">
           <img src={iconLike} alt="heart" />
           <p>123</p>
+          </div>
+
           <div>Type: {recipeDetail.mealType} </div>
           <div>Difficulty: {recipeDetail.difficulty} </div>
           <div>Time: {recipeDetail.time}</div>
         </div>
 
         <div className="flex flex-col justify-center mb-4">
-        <p className="font-medium flex justify-center mt-2">Description</p>
-        <p className="flex justify-center align-items-center">{recipeDetail.description}</p>
+        <p className="font-medium flex justify-center mt-4">Description</p>
+        <p className="flex text-center justify-center align-items-center mr-3 ml-3">{recipeDetail.description}</p>
         </div>
 
-        <div className="flex flex-col">
-          <div className="flex flex-row gap-2 mb-1">
+        <div className="flex flex-col mt-16 w-full items-center mr-3 ml-3">
+          <div className="flex flex-row gap-2 mb-3">
           <img className="w-5"src={Comment} alt="Comment cloud"/>
           <h1>Comments</h1>
           </div>
 
-          <div className="flex flex-row">
-          <img className="w-7" src={man1} alt="comment-guy" />
-          <p>Tasty, fresh, spicy and nutritious dish. I tried it first in Peru and then I made it at home. I can't get enough of it! You shouldn't miss it!</p> 
+          <div className="flex flex-row mb-3 max-w-lg mr-3 ml-3">
+          <img className="h-11" src={man1} alt="comment-guy" />
+          <p className="ml-2">Tasty, fresh, spicy and nutritious dish. I tried it first in Peru and then I made it at home. I can't get enough of it! You shouldn't miss it!</p> 
           </div>
-
-          <input type="text" placeholder="write here..." />
+          <input className="flex w-1/2 h-8 bg-gray-light" type="text" placeholder="write here..." />
+          
         </div>
 
       </div>
